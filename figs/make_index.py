b9d7a4a92a0bd2ad03704890b08875e11ca91935
Xinyu Wei <trustywolf@wide.ad.jp>
2023-12-06 01:36:24 +0900

    2023-12-06T01:36:24+09:00

---
+++ figs/make_index.py
@@ -0,0 +1,35 @@
+
+import os
+import json
+from mako.template import Template
+from mako.lookup import TemplateLookup
+import glob
+import re
+
+def make_index():
+    output_file = '../html/figs/index.html'
+
+    ary = []
+    for filepath in sorted(glob.glob('../html/figs/*000.html')):
+        m = re.match(r'../html/figs/((\d+)\.html)', filepath)
+        if m:
+            filename  = m[1]
+            rfc_begin = int(m[2])
+            rfc_end   = rfc_begin + 999
+            ary.append((filename, rfc_begin, rfc_end))
+
+    if len(ary) == 0:
+        print('make_index: Not found html files')
+
+    mylookup = TemplateLookup(
+        directories=["./"],
+        input_encoding='utf-8', output_encoding='utf-8')
+    mytemplate = mylookup.get_template('templates/index.html')
+    output = mytemplate.render_unicode(ctx=ary)
+
+    with open(output_file, 'w') as f:
+        f.write(output)
+
+
+if __name__ == '__main__':
+    make_index()
